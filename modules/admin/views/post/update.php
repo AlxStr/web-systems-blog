b9740f4434940b5d6cb7b1329e499667c94f7799
Alex Strebchuk <alex.strebchuk@gmail.com>
2019-02-12 00:42:34 +0200

    Redone post update view in admin & client

--- modules/admin/views/post/update.php
+++ modules/admin/views/post/update.php
@@ -1,6 +1,8 @@
 <?php
 
+use dosamigos\ckeditor\CKEditor;
 use yii\helpers\Html;
+use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Post */
@@ -12,11 +14,48 @@ $this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="post-update">
 
-    <h1><?= Html::encode($this->title) ?></h1>
 
-    <?= $this->render('_form', [
-        'model' => $model,
-        'categories' => $categories,
-    ]) ?>
+    <?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']]); ?>
+
+    <div class="box box-default">
+        <div class="box-body">
+            <div class="row">
+                <div class="col-md-8">
+                    <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+                </div>
+                <div class="col-md-4">
+                    <?= $form->field($model, 'category_id')->dropDownList($categories) ?>
+                </div>
+
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <?= $form->field($model, 'description')->textarea(['rows' => 15]) ?>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <?= $form->field($model, 'body')->widget(CKEditor::className(), [
+                        'options' => ['rows' => 6],
+                        'preset' => 'custom',
+                        'clientOptions' => [
+                            'toolbarGroups' => [
+                                ['name' => 'undo'],
+                                ['name' => 'insert', 'groups' => ['insert']],
+                            ],
+                        ],
+                    ]) ?>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
 
 </div>
