58a83634295c587804cc0f812faca85d31e5435a
Alex Strebchuk <alex.strebchuk@gmail.com>
2019-03-24 22:29:47 +0200

    change _data token

--- tests/_data/token.php
+++ tests/_data/token.php
@@ -2,12 +2,12 @@
 return [
     [
         'user_id' => 1,
-        'token' => 'token-correct',
+        'token' => 'admin-token-correct',
         'expired_at' => time() + 3600,
     ],
     [
         'user_id' => 1,
-        'token' => 'token-expired',
+        'token' => 'admin-token-expired',
         'expired_at' => time() - 3600,
     ],
     [
